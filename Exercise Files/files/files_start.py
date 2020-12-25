9e081c8473ad1813aa3550f0c6af00cb09969bed
luiscasado620@gmail.com <Luiscasado620@gmail.com>
2020-12-25 10:03:54 -0500

    New files

---
+++ Exercise Files/files/files_start.py
@@ -0,0 +1,15 @@
+def main():
+    f = open('text.txt', 'r')
+
+    # for i in range(10):
+    #     f.write("This is line "+ str(i) + '\r\n')
+
+    # f.close()
+
+    if f.mode == 'r':
+        contents = f.read()
+        print(contents)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Exercise Files/files/osptahutils.py
@@ -0,0 +1,27 @@
+import os
+from os import path
+import datetime
+from datetime import date, time, timedelta
+import time
+
+
+
+def main():
+    print(os.name)
+
+    # print("Item Path: " + str(path.exists("text.txt")))
+    # print(path.isfile('text.txt'))
+    # print(path.isdir('text.txt'))
+
+    print("Item path: " + str(path.realpath('text.txt')))
+    print("Item pth and name: " + str(path.split(path.realpath('text.txt'))))
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
