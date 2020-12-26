d8d0a99de0105bb9ac254d1c5441363273980293
luiscasado620@gmail.com <Luiscasado620@gmail.com>
2020-12-26 09:45:17 -0500

    chap 10

--- Python Essential Training/Ex_Files_Python_EssT/Exercise Files/Chap11/hello.py
+++ Python Essential Training/Ex_Files_Python_EssT/Exercise Files/Chap11/hello.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.')
+
+class MyString(str):
+    def __str__(self):
+        return self[::-1]
+
+
+
+x = ('Hello, World {}'.upper().format(34*8))
+print(x)
