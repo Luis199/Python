4b59de61f551eb7d4fbd2cc9b053986b732aef66
luiscasado620@gmail.com <Luiscasado620@gmail.com>
2020-12-25 21:24:51 -0500

    New Files

---
+++ Python Essential Training/Ex_Files_Python_EssT/Exercise Files/Chap01/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+from decimal import * 
+
+x = 7/2
+y = "Hello World"
+print(type(x))
+print(type(y))
+
+print('x is {}'.format(x))
+print('y is {}'.format(y))
