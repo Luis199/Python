2afbd9e392615a83f91b448e1463701b0c55ac45
luiscasado620@gmail.com <Luiscasado620@gmail.com>
2021-01-07 14:35:39 -0500

    items

---
+++ algorithms/stack.py
@@ -0,0 +1,15 @@
+from collections import deque
+stack = []
+
+for i in range(5):
+    stack.append(i)
+
+queue = deque()
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+
+
+
+print(queue)
