1f1817cd6219e4c45a9a051942a2709c003b6228
luiscasado620@gmail.com <Luiscasado620@gmail.com>
2021-01-09 23:32:53 -0500

    New Data

---
+++ standard_library/manipulating_data/sorting.py
@@ -0,0 +1,7 @@
+testScores = [98,100,90,95,97,92,99]
+
+sortedScores = sorted(testScores)
+print(sortedScores)
+
+sortedScores = sorted(testScores, reverse=True)
+print(sortedScores)
--- standard_library/strings_text/string_format.py
+++ standard_library/strings_text/string_format.py
@@ -1,6 +1,10 @@
 from string import Template
+import datetime
 
 sample_str = "This is $going $great year"
-the_template = Template(sample_str)
-output_str = the_template.substitute(going='fox', great='jumped')
-print(output_str)
+# the_template = Template(sample_str)
+# output_str = the_template.substitute(going='fox', great='jumped')
+# print(output_str)
+
+nyd = datetime.datetime(2021,1,8)
+print(nyd)
